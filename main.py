dc168ce2a439b703440cad47b471d8bfbe50b4b9
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-10-17 18:25:28 +0530

    threadpool executer

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 from transformers import pipeline
 import asyncio
 import uvicorn
 
 app = FastAPI()
+
 # Use a global variable for the model
 global_model = None
 
@@ -17,19 +18,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Initialize the model when the FastAPI app starts and reuse it for each request
+async def initialize_and_get_model():
+    global global_model
+    if global_model is None:
+        loop = asyncio.get_event_loop()
+        global_model = await loop.run_in_executor(pool, initialize_model)
+    return global_model
+
 def initialize_model():
-    return pipeline('fill-mask',model='nlpaueb/legal-bert-base-uncased',tokenizer='nlpaueb/legal-bert-base-uncased')
+    return pipeline('fill-mask', model='nlpaueb/legal-bert-base-uncased', tokenizer='nlpaueb/legal-bert-base-uncased')
 
-# Initialize the process pool executor with an initializer function
-pool = ProcessPoolExecutor(max_workers=1)
+# Initialize the thread pool executor with an initializer function
+pool = ThreadPoolExecutor(max_workers=1)
 
-async def generate_response(prompt):
+async def generate_response(prompt, model=Depends(initialize_and_get_model)):
     words = prompt.split()
     for i, k in enumerate(words):
         if k[0] == '<' and k[-1] == '>':
             words[i] = '[MASK]'
     encoded_text = ' '.join(map(str, words))
-    pred = global_model(encoded_text)
+    pred = model(encoded_text)
     words = encoded_text.split()
     token_replace_count = 0
     for i, word in enumerate(words):
@@ -39,27 +48,16 @@ async def generate_response(prompt):
     prompt_generated = ' '.join(map(str, words))
     return prompt_generated
 
-@app.on_event("startup")
-async def startup_event():
-    global global_model
-    loop = asyncio.get_event_loop()
-    # Initialize the model when the FastAPI app starts
-    global_model = await loop.run_in_executor(pool, initialize_model)
-
-
 @app.post('/prompt')
-async def process(request: Request):
+async def process(request: Request, model=Depends(initialize_and_get_model)):
     # Get the input data from the POST request
     data = await request.json()
 
     # Check if 'prompt' is in the JSON data
     if 'prompt' in data:
         prompt = data['prompt']
-
-        if global_model is None:
-            raise HTTPException(status_code=500, detail="Model is not initialized")
         # Use the global model for prediction
-        prompt_generated = await generate_response(prompt)
+        prompt_generated = await generate_response(prompt, model)
         return {'response': prompt_generated}
 
     else:
